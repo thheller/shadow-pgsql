d7a88ca3713f4cb7096587d1937bb6889799df9f
Thomas Heller <info@zilence.net>
2014-08-13 16:13:21 +0200

    alright ... tests here I come ...

--- test/java/shadow/pgsql/BasicTest.java
+++ test/java/shadow/pgsql/BasicTest.java
@@ -1,56 +1,58 @@
 package shadow.pgsql;
 
+import org.junit.*;
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 /**
- * haha, you fool. There are no tests here.
+ * alright ... this is a very basic test.
  */
 public class BasicTest {
+    private Connection pg;
+    private Database db;
 
-    public static void main(String[] args) throws IOException {
-        try (Connection pg = Database.setup("localhost", 5432, "zilence", "shadow_pgsql")
-                .connect()) {
+    @Before
+    public void setupConnection() throws Exception {
+        this.db = Database.setup("localhost", 5432, "zilence", "shadow_pgsql");
+        this.pg = db.connect();
 
-            pg.execute("DELETE FROM num_types");
+        pg.execute("DELETE FROM num_types");
+    }
 
-            pg.begin();
+    @After
+    public void closeConnection() throws Exception {
+        this.pg.close();
+    }
 
-            try (PreparedStatement stmt = pg.prepare("INSERT INTO num_types (fint8) VALUES ($1)")) {
-                stmt.execute(1);
-                Savepoint savepoint = pg.savepoint();
-                stmt.execute(2);
-                savepoint.rollback();
-                stmt.execute(3);
-            }
+    @Test
+    public void testTransactionWithSavepoint() throws IOException {
+        pg.begin();
 
-            pg.commit();
-
-            SimpleQuery query = new SimpleQuery("SELECT fint8 FROM num_types");
-            query.setRowBuilder(Handlers.SINGLE_COLUMN);
-
-            // wait ... this is starting to look like a test
-            List numTypes = (List) pg.executeQuery(query);
-            boolean n1 = numTypes.contains(1l);
-            boolean n2 = numTypes.contains(2l);
-            boolean n3 = numTypes.contains(3l);
-
-            /*
-            // basic INSERT
-            long rows = pg.execute("INSERT INTO num_types (fint4) VALUES ($1)", 1).getRowsAffected();
-
-            // INSERT with SERIAL id, returns generated id
-            SimpleQuery query = new SimpleQuery(
-                    "INSERT INTO num_types (fint4) VALUES ($1) RETURNING id"
-            );
-
-            query.setRowBuilder(Handlers.SINGLE_COLUMN);
-
-            int insertedId = (int) pg.executeQuery(query, 1);
-
-            */
-
-            System.out.println("Fancy.");
+        try (PreparedStatement stmt = pg.prepare("INSERT INTO num_types (fint8) VALUES ($1)")) {
+            stmt.execute(1);
+            Savepoint savepoint = pg.savepoint();
+            stmt.execute(2);
+            savepoint.rollback();
+            stmt.execute(3);
         }
+
+        pg.commit();
+
+        SimpleQuery query = new SimpleQuery("SELECT fint8 FROM num_types");
+        query.setRowBuilder(Handlers.SINGLE_COLUMN);
+
+        List numTypes = (List) pg.executeQuery(query);
+        assertTrue(numTypes.contains(1l));
+        assertFalse(numTypes.contains(2l));
+        assertTrue(numTypes.contains(3l));
+    }
+
+    @Test
+    public void testSimpleQuery() throws IOException {
+        List<Map<String, Object>> results = (List<Map<String, Object>>) pg.executeQuery("SELECT * FROM num_types");
+        System.out.println("yo");
     }
 }
