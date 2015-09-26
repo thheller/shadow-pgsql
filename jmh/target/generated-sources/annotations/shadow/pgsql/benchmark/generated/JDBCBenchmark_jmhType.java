b8cb7f1dda8aefe3ee3e4bd8c19fc970e1e1cf7d
Thomas Heller <info@zilence.net>
2015-09-26 15:54:47 +0200

    oops

--- jmh/target/generated-sources/annotations/shadow/pgsql/benchmark/generated/JDBCBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package shadow.pgsql.benchmark.generated;
-public class JDBCBenchmark_jmhType extends JDBCBenchmark_jmhType_B3 {
-}
-
--- jmh/target/generated-sources/annotations/shadow/pgsql/benchmark/generated/ShadowBenchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package shadow.pgsql.benchmark.generated;
-public class ShadowBenchmark_jmhType extends ShadowBenchmark_jmhType_B3 {
-}
-
