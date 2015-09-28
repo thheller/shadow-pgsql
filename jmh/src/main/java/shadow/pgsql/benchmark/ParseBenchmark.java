444da114c1a42bb14123205b180fd9c7059114f5
Thomas Heller <info@zilence.net>
2015-09-28 12:03:21 +0200

    extra benchmark to verify expectation

--- jmh/src/main/java/shadow/pgsql/benchmark/ParseBenchmark.java
+++ jmh/src/main/java/shadow/pgsql/benchmark/ParseBenchmark.java
@@ -19,6 +19,11 @@ public class ParseBenchmark {
         SQL.parseParamCount(testSQL);
     }
 
+    @Benchmark
+    public void parseToCharArray() {
+        parseParamCountCopy(testSQL);
+    }
+
     private static final Pattern PARAM_PATTERN = Pattern.compile("\\$([0-9]+)", Pattern.MULTILINE | Pattern.DOTALL);
 
     public static int parseParamCountRegexp(final String sql) {
@@ -32,9 +37,46 @@ public class ParseBenchmark {
         return paramCount;
     }
 
+    public static int parseParamCountCopy(final String sql) {
+        // creates a copy of the internal array
+        char[] chars = sql.toCharArray();
+        int length = sql.length();
+        int paramCount = 0;
+
+        boolean param = false;
+        int current = 0;
+        for (int i = 0; i < length; i++) {
+            char c = chars[i];
+
+            if (param) {
+                if (c >= 48 && c <= 57) {
+                    current = (current * 10) + (c - 48);
+
+                    if (current > 65536) {
+                        throw new IllegalStateException("exceeded maximum parameter count");
+                    }
+                } else {
+                    param = false;
+                    paramCount = Math.max(paramCount, current);
+                    current = 0;
+                }
+            } else if (c == '$') {
+                param = true;
+            }
+        }
+
+        // ends in param digits
+        if (param) {
+            paramCount = Math.max(paramCount, current);
+        }
+
+        return paramCount;
+    }
+
     // expected regexp to be slow but not by that much
-    // ParseBenchmark.parseManual  thrpt   20  20384728,548 ± 126483,398  ops/s
-    // ParseBenchmark.parseRegexp  thrpt   20   1746319,678 ±   9665,410  ops/s
+    // ParseBenchmark.parseManual       thrpt   20  20066966,291 ± 274651,720  ops/s
+    // ParseBenchmark.parseRegexp       thrpt   20   1739917,590 ±   9985,324  ops/s
+    // ParseBenchmark.parseToCharArray  thrpt   20  15097267,034 ± 190592,764  ops/s
 
     @Benchmark
     public void parseRegexp() {
